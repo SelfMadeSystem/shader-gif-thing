a2ee1c56218cfb080f326a638b241e9a6725dfe8
SelfMadeSystem <sms@shoghisimon.cc>
2025-01-16 12:58:21 -0500

    fix: update GIF encoding options for improved palette generation and compression

--- utils.ts
+++ utils.ts
@@ -41,15 +41,13 @@ export function encodeFramesToGif(
     const command = ffmpeg(ffmpegStream)
       .inputFormat("rawvideo")
       .inputOptions(["-pix_fmt rgba", `-s ${width}x${height}`, `-r ${fps}`])
-      /* 
       .outputOptions([
-        "-vf", `split[x][y];[x]palettegen=max_colors=256:stats_mode=single[p];[y][p]paletteuse=dither=sierra2_4a:diff_mode=rectangle`,
+        "-vf", `split[x][y];[x]palettegen=max_colors=256:stats_mode=full[p];[y][p]paletteuse=dither=bayer:bayer_scale=3:diff_mode=rectangle`,
         `-s ${width}x${height}`,
         "-q:v", "1",
-        "-compression_level", "9"
+        "-compression_level", "10"
       ])
-      */
-      .outputOptions(["-vf", `scale=${width}:${height}`, "-pix_fmt", "rgb24"])
+      // .outputOptions(["-vf", `scale=${width}:${height}`, "-pix_fmt", "rgb24"])
       .output(outputFile)
       .on("end", () => {
         console.log("Encoding finished.");
